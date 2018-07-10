4f69e34429018ab8c10dc96d021a81b7f09099a4
Vasilis Kortsimelidis <vasilisk@asos.com>
2018-07-10 10:04:05 +0100

    Strapi docker setup

---
+++ strapi.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -ea
+
+_stopStrapi() {
+  echo "Stopping strapi"
+  kill -SIGINT "$strapiPID"
+  wait "$strapiPID"
+}
+
+trap _stopStrapi SIGTERM SIGINT
+
+cd /usr/src/api
+
+APP_NAME=${APP_NAME:-strapi-app}
+DATABASE_AUTHENTICATION=${AUTHENTICATION_DATABASE:-false}
+DATABASE_ENABLE_SSL=${DATABASE_ENABLE_SSL:-false}
+DATABASE_CLIENT=${DATABASE_CLIENT:-mongo}
+DATABASE_HOST=${DATABASE_HOST:-localhost}
+DATABASE_PORT=${DATABASE_PORT:-27017}
+DATABASE_NAME=${DATABASE_NAME:-strapi}
+
+if [ ! -f "$APP_NAME/package.json" ]
+then
+    strapi new ${APP_NAME} --dbclient=$DATABASE_CLIENT --dbhost=$DATABASE_HOST --dbport=$DATABASE_PORT --dbname=$DATABASE_NAME --dbusername=$DATABASE_USERNAME --dbpassword=$DATABASE_PASSWORD --dbauthentication=$AUTHENTICATION_DATABASE --dbssl=$DATABASE_ENABLE_SSL
+fi
+
+cd $APP_NAME
+strapi start &
+
+strapiPID=$!
+wait "$strapiPID"
