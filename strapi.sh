f184a3929238275ab0c9b1e55d3b3563f89b3fae
ActiveEvil <active_evil@protonmail.com>
2018-08-03 14:14:45 +0100

    Prepare CMS for non-containerized deployment

--- strapi.sh
+++ strapi.sh
@@ -9,20 +9,7 @@ _stopStrapi() {
 
 trap _stopStrapi SIGTERM SIGINT
 
-# if [ ! -f "$APP_NAME/package.json" ]
-# then
-#     strapi -v
-#     strapi new $APP_NAME --dbclient=$DATABASE_CLIENT --dbhost=$DATABASE_HOST --dbport=$DATABASE_PORT --dbname=$DATABASE_NAME --dbusername=$DATABASE_USERNAME --dbpassword=$DATABASE_PASSWORD
-# fi
-#
-# # replace database configs from strapi - there's a bug
-# cp database.json $APP_NAME/config/environments/production/database.json
-# cp database.json $APP_NAME/config/environments/staging/database.json
-# cp database.json $APP_NAME/config/environments/development/database.json
-
-# cp server.js $APP_NAME/server.js
-
-cd $APP_NAME
+cd app
 
 npm install
 
